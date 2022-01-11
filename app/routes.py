0b693cea311e32fc10512462b700f4055f317458
Gary Galvin <grgalvin0104@gmail.com>
2022-01-11 12:04:00 -0800

    locate individual user

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
-from flask import Flask, request
+import requests
+from flask import request
 from datetime import datetime
 
-app = Flask(__name__)
-VERSION = "1.0.0"
+from app import app, db
+from app.database import User
 
-from app.database import user
+VERSION = "1.0.0"
 
 @app.get("/version")
 def get_version():
@@ -15,48 +16,63 @@ def get_version():
     return out
 
 
+
+
 @app.post("/users")
 def creat_user():
     user_data = request.json
-    new_id = user.insert(
-        user_data.get("first_name"),
-        user_data.get("last_name"),
-        user_data.get("hobbies"),)
-
-    out = {"new_id": new_id}
-    return out, 201
+    db.session.add(
+        User(
+            first_name=user_data.get("first_name"),
+            last_name=user_data.get("last_name"),
+            hobbies=user_data.get("hobbies")
+        )
+    )
+    db.session.commit()
+    return {"status": "success"}, 201
 
 @app.get("/users")
 def get_all_users():
-    users = user.scan()
-    out = {"users": users}
-    return out
+    users = User.query.all()
+    out_list = []
+    for user in users:
+        temp = {}
+        temp["first_name"]= user.first_name
+        temp["last_name"] = user.last_name
+        temp["hobbies"] = user.hobbies
+        temp["active"] = user.active
+        out_list.append(temp)
+    return {"users": out_list}
 
 @app.get("/users/<int:pk>")
 def get_single_user(pk):
-    user_record = user.read(pk)
-    out = {"user": user_record}
-    return out
+    user = User.query.filter_by(id=pk).first()
 
-@app.put("/users/<int:pk>")
-def put_single_user(pk):
-    user_data = request.json
-    user.update(
-    pk,
-    user_data.get("first_name"),
-    user_data.get("last_name"),
-    user_data.get("hobbies")
-    )
+    return {
+        "first_name": user.first_name,
+        "last_name" : user.last_name,
+        "hobbies" : user.hobbies
+    }
 
-    return "ok", 204
+# @app.put("/users/<int:pk>")
+# def put_single_user(pk):
+#     user_data = request.json
+#     user.update(
+#     pk,
+#     user_data.get("first_name"),
+#     user_data.get("last_name"),
+#     user_data.get("hobbies")
+#     )
+
+#     return "ok", 204
     
 
 
-@app.delete("/users/<int:pk>")
-def delete_single_user(pk):
-    user.deactivate_user(pk) 
+# @app.delete("/users/<int:pk>")
+# def delete_single_user(pk):
+#     user.deactivate_user(pk) 
 
-    return "OK", 204
+#     return "OK", 204
 
 # FOR UPDATE: an HTTP PUT operation with rout: /users/<int:pk> 
 # FOR DEACTIVATE: an HTTP DELETE operation with route: /user/<int:pk>
